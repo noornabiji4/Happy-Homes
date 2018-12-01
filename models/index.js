7e76decc5de4e6856c1359dc2a7555443b5e5bf8
Noor <noornabiji4@gmail.com>
2018-12-01 21:57:12 +0530

    schema

--- models/index.js
+++ models/index.js
@@ -1,12 +1,15 @@
-const mongoose =require('mongoose');
-mongoose.connect('mongodb://localhost/happyhomes',(err)=>{ //enter ur api name
-    if(err){
-        console.log(err);
-    }else {
-        console.log("Database is connceted");
-    } 
-});
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/happyhomes",
+  err => {
+    //enter ur api name
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database is connceted");
+    }
+  }
+);
 
-module.exports.Pg = require('./pg')
+module.exports.Pg = require("./pg");
 
 mongoose.Promise = Promise;
