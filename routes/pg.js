49eb87f73ffcd9906c8d4240176da398a410b689
Noor <noornabiji4@gmail.com>
2018-12-01 21:59:56 +0530

    pg crud

--- routes/pg.js
+++ routes/pg.js
@@ -1,44 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../models');
+const db = require("../models");
 
-
-//pg 
-router.get('/', (req, res) => {
-    db.Pg.find()
-        .then((pg) => res.json(pg))
-        .catch((err) => res.send(err));
+//pg
+router.get("/", (req, res) => {
+  db.Pg.find()
+    .then(pg => res.json(pg))
+    .catch(err => res.send(err));
 });
 
 // pg home route
-router.post('/' ,(req, res) => {
-    db.Pg.create(req.body)
-        .then(res.redirect('/home'))
-        .catch((err) => res.send(err))
+router.post("/", (req, res) => {
+  db.Pg.create(req.body)
+    .then(res.redirect("/home"))
+    .catch(err => res.send(err));
 });
 
 // pg show
-router.get('/:id', (req, res) => {
-    let id = req.params.id;
-    db.Pg.findById(id)
-        .then((pg) => res.json(pg))
-        .catch((err) => res.send(err))
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
+  db.Pg.findById(id)
+    .then(pg => res.json(pg))
+    .catch(err => res.send(err));
 });
 
 // update pg
-router.put('/:id', (req, res) => {
-    db.Pg.findByIdAndUpdate({ _id:req.params.id }, req.body)
-        .then((pg) => res.json(pg))
-        .catch((err) => res.send(err))
+router.put("/:id", (req, res) => {
+  db.Pg.findByIdAndUpdate({ _id: req.params.id }, req.body)
+    .then(pg => res.json(pg))
+    .catch(err => res.send(err));
 });
 
 //delete pg
 
-router.delete('/:id', (req, res) => {
-    db.Pg.remove({ _id:req.params.id})
-        .then(res.send("removed"))
-        .catch((err) => res.send(err))
+router.delete("/:id", (req, res) => {
+  db.Pg.remove({ _id: req.params.id })
+    .then(res.send("removed"))
+    .catch(err => res.send(err));
 });
 
-
-module.exports = router; 
+module.exports = router;
