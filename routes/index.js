0231a9bb2d78b33c8aabcebbc667dcce5e4372ec
Noor <noornabiji4@gmail.com>
2018-12-01 21:59:31 +0530

    index router

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
-router.get('/home',(req,res)=>{
-  res.render('pg');
-}); 
-router.get('/updatepg',(req,res)=>{
-  res.render('updatepg');
+router.get("/home", (req, res) => {
+  res.render("pg");
+});
+router.get("/updatepg", (req, res) => {
+  res.render("updatepg");
 });
 module.exports = router;
