5bacce91efb40d4667e404d7a7de5882860b96bf
Noor <noornabiji4@gmail.com>
2018-12-03 18:43:07 +0530

    update

--- models/index.js
+++ models/index.js
@@ -1,14 +1,22 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/happyhomes",
-  err => {
-    //enter ur api name
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("Database is connceted");
-    }
-  }
-);
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds123844.mlab.com:23844/thshappyhomes');
+
+// Set up default mongoose connection
+const mongoDB = 'mongodb://noor:noor123@ds123844.mlab.com:23844/thshappyhomes'
+
+mongoose.connect(mongoDB);
+
+// for local machine mongo server
+// mongoose.connect("mongodb://localhost/happyhomes",
+//   err => {
+//     //enter ur api name
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Database is connceted");
+//     }
+//   }
+// );
 
 module.exports.Pg = require("./pg");
 
